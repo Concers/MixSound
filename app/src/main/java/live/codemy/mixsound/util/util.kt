b67a74eee970f04021a1ddb864cd04e2e0de1fc9
Uğur Erol <info@geceugurerol.com>
2020-02-14 01:05:09 +0300

    // Image Commit

---
+++ app/src/main/java/live/codemy/mixsound/util/util.kt
@@ -0,0 +1,18 @@
+package live.codemy.mixsound.util
+
+import android.util.Log
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║  Created by Uğur Erol  ║
+╠════════════════════════════╣
+║ info@geceugurerol.com ║
+╠════════════════════════════╣
+║     12/02/2020 - 23:12     ║
+╚════════════════════════════╝
+ */
+
+fun String.logError() {
+    Log.e("MainActivity", this)
+}
